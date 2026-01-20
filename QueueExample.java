40212e29f307d0d8aabd0809fbd6e8caacc18e4d
TessaMJ <161723842+TessaMJ@users.noreply.github.com>
2026-01-20 09:21:57 +0530

    Queue

---
+++ QueueExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Geeks {
+
+    public static void main(String args[])
+    {
+        Queue<String> pq = new PriorityQueue<>();
+
+        pq.add("Programming");
+        pq.add("Java");
+        
+
+        System.out.println(pq);
+    }
+}
